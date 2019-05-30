03b9888023ce9d35e075fbbb6d765a999567d171
Srishti Sharma <38854039+Srishti325@users.noreply.github.com>
2019-05-30 16:46:20 -0700

    Add files via upload

---
+++ dec to bin.py
@@ -0,0 +1,13 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+dec = 123
+
+print("The decimal value of",dec,"is:")
+print(bin(dec),"in binary.")
+print(oct(dec),"in octal.")
+print(hex(dec),"in hexadecimal.")
+
